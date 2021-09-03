f8613c99a9fe5bc19debd96819be08ebb93f9971
cbass2404 <cory.bass.07@gmail.com>
2021-09-03 11:00:08 -0500

    completed second exercise

--- 1-ide-greeter.ts
+++ 1-ide-greeter.ts
@@ -1,3 +1,5 @@
 function greeter(person: string) {
-	return "Hello, " + person;
+  return "Hello, " + person;
 }
+
+export default greeter;
---
+++ 1-ide-hello-earth.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var _1_ide_greeter_1 = require("./1-ide-greeter");
+document.body.innerHTML = (0, _1_ide_greeter_1["default"])("Earth");
